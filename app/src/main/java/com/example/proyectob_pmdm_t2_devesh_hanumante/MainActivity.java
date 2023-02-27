9c2f112b3c1d89e1fb5eca86dd805c43924a1f12
deveshaha <devesh_hanumante@outlook.com>
2023-02-27 11:17:20 +0100

    project upload

---
+++ app/src/main/java/com/example/proyectob_pmdm_t2_devesh_hanumante/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.proyectob_pmdm_t2_devesh_hanumante;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
